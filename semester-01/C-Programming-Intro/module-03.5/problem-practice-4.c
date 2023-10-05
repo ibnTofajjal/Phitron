b4ac756b763a12c23d0a048f92b0b15b8d76fe91
Joynal Bin Tofajjal <67532018+ibnTofajjal@users.noreply.github.com>
2023-10-05 12:39:52 +0600

    Update problem-practice-4.c

--- semester-01/C-Programming-Intro/module-03.5/problem-practice-4.c
+++ semester-01/C-Programming-Intro/module-03.5/problem-practice-4.c
@@ -3,13 +3,15 @@
 int main() {
     
     int A =100;
-    long long int B = 200000000000000;
-    float C = 10.065;
+    long long int B = 1234567891234567;
+    float C = 23.5675;
     char D = 'A';
 
     printf("%d\n", A);
     printf("%lld\n", B);
     printf("%.2f\n", C);
     printf("%c\n", D);
+        
     return 0;
+    
 }
