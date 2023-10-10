6ee156235c6f31ed2be5e70f4ed60fece01124cc
ibnTofajjal <joynal.bintofajjal@gmail.com>
2023-10-11 00:58:01 +0600

    Module - 07

--- semester-01/C-Programming-Intro/module-06.5/C_Next_Alphabet.c
+++ semester-01/C-Programming-Intro/module-06.5/C_Next_Alphabet.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
 int main () {
+    char n;
+    scanf("%c", &n);
 
+    if(n >= 'a' && n < 'z'){
+        char nextAlpha = n + 1;
+        printf("%c", nextAlpha);
+    } else if (n == 'z')
+    {
+        printf("a");
+    }
     
 
     return 0;
