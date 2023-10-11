627b4109d56aabea4c6ef04f96d29ca3f717f8bc
ibnTofajjal <joynal.bintofajjal@gmail.com>
2023-10-12 01:58:28 +0600

    Module - 07.5

--- semester-01/C-Programming-Intro/module-06.5/C_Next_Alphabet.c
+++ semester-01/C-Programming-Intro/module-06.5/C_Next_Alphabet.c
@@ -15,3 +15,6 @@ int main () {
 
     return 0;
 }
+
+
+// submitted : Done
--- semester-01/C-Programming-Intro/module-06.5/K_Divisors.c
+++ semester-01/C-Programming-Intro/module-06.5/K_Divisors.c
@@ -2,7 +2,20 @@
 
 int main () {
 
+    int n;
+    scanf("%d", &n);
     
-
+    for (int i = 1; i <= 12; i++)
+    {
+        if (n % i == 0)
+        {
+            printf("%d\n", i);
+        }
+        
+    }
+    
     return 0;
 }
+
+
+// Test 4 : wrong 
